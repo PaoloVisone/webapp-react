705080eba07dd6e446b6577e51b3ca22b97513d5
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-12 18:44:36 +0100

    [feat] import Card

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,10 +1,18 @@
+// Card
+import Card from "../components/Card"
+
 const HomePage = () => {
     return (
         <>
             <h1 className="text-dark">Bool Movie</h1>
 
             <div className="row row-cols-3 mt-4">
-                <h1>Movie</h1>
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
+                <Card />
             </div>
         </>
 
