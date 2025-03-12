c2270f41e4203a276c9a7a8faab851e951729e59
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-12 18:45:55 +0100

    [feat] import component and add section Reviews. Add Footer with button

--- src/pages/MoviePage.jsx
+++ src/pages/MoviePage.jsx
@@ -1,12 +1,35 @@
+// Importo react-router
+import { Link } from "react-router-dom"
+
+import Reviews from "../components/Reviews"
+
 const MoviePage = () => {
     return (
-        <header id="movie" className="border-bottom border-1 mb-3">
-            <h1>Titolo</h1>
-            <h3 className="text-muted">
-                <i>By </i>
-            </h3>
-            <p>Lorem ipsum dolor sit amet.</p>
-        </header>
+        <>
+            <section id="movie" className="border-bottom border-1 d-flex mb-3">
+                <div className="d-flex mb-3">
+                    <img src="http://localhost:3000/movies_cover/inception.jpg" class="movie-img" alt="..." />
+                </div>
+                <div className="text p-4 ">
+                    <h1>Titolo</h1>
+                    <h3 className="text-muted">
+                        <i>By </i>
+                    </h3>
+                    <p>Lorem ipsum dolor sit amet.</p>
+                </div>
+            </section>
+
+            <section id="reviews" className="mb-4">
+                <h4>Reviews</h4>
+                <Reviews />
+                <Reviews />
+                <Reviews />
+            </section>
+
+            <footer className="border-top border-1 pt-2 mb-3 d-flex justify-content-end">
+                <Link className="btn btn-secondary" to="/">Back to home</Link>
+            </footer>
+        </>
 
     )
 }
