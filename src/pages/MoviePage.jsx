24f89204974313d02b062decc4484f504a3beaaf
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-12 17:31:44 +0100

    [feat] add MoviePage

---
+++ src/pages/MoviePage.jsx
@@ -0,0 +1,14 @@
+const MoviePage = () => {
+    return (
+        <header id="movie" className="border-bottom border-1 mb-3">
+            <h1>Titolo</h1>
+            <h3 className="text-muted">
+                <i>By </i>
+            </h3>
+            <p>Lorem ipsum dolor sit amet.</p>
+        </header>
+
+    )
+}
+
+export default MoviePage
