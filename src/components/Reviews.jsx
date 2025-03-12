475503a08bed731e9c0d1208ca09d44a6f83cb89
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-12 18:44:08 +0100

    [feat] add component Reviews

---
+++ src/components/Reviews.jsx
@@ -0,0 +1,16 @@
+const Reviews = () => {
+
+    return (
+
+        <div className="card mb4">
+            <div className="card-body">
+                <p className="card-text">testo</p>
+                <strong>Vote: voto</strong>
+                <address><i>By</i></address>
+            </div>
+        </div>
+
+    )
+}
+
+export default Reviews
