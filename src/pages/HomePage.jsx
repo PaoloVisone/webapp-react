ddc4703c7446755605da767a1c76554aad060dd6
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-13 02:08:04 +0100

    [feat] import Axios, import useState, useEffect. .map in rendering function

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,18 +1,51 @@
+// UseState e useEffect
+import { useState, useEffect } from "react"
+
+// Axios
+import axios from "axios"
+
 // Card
 import Card from "../components/Card"
 
 const HomePage = () => {
+
+    // Stato del componente
+    const [movies, setMovies] = useState([])
+
+    // Chiamata al DB
+    const getData = () => {
+        axios.get("http://localhost:3000/api/movies/")
+            .then(
+                res => {
+                    // Cambia lo stato
+                    setMovies(res.data)
+                }
+            )
+            .catch(err => console.log(err))
+    }
+
+    // Al rendering faccio partire la richiesta dati
+    useEffect(getData, [])
+
+    // Rendering Function
+    const renderMovies = () => {
+        return movies.map(
+            movie => {
+                return (
+                    <div className="col">
+                        <Card propCard={movie} key={movie.id} />
+                    </div>
+                )
+            }
+        )
+    }
+
     return (
         <>
-            <h1 className="text-dark">Bool Movie</h1>
+            <h1 className="text-dark text-center">Bool Movie</h1>
 
             <div className="row row-cols-3 mt-4">
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
-                <Card />
+                {renderMovies()}
             </div>
         </>
 
