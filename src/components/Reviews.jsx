1beb035387c38dc3dbdafcdb2ec38ce3679e0c4e
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-13 19:47:35 +0100

    [feat] add props with destructuring

--- src/components/Reviews.jsx
+++ src/components/Reviews.jsx
@@ -1,12 +1,15 @@
-const Reviews = () => {
+const Reviews = ({ propReviews }) => {
+
+    // destructuring delle props
+    const { id, name, vote, text } = propReviews
 
     return (
 
         <div className="card mb4">
             <div className="card-body">
-                <p className="card-text">testo</p>
-                <strong>Vote: voto</strong>
-                <address><i>By</i></address>
+                <p className="card-text">{text}</p>
+                <strong>Vote: {vote}</strong>
+                <address><i>By {name}</i></address>
             </div>
         </div>
 
