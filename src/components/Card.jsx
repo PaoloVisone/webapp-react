99222053483621e342ba5f13ac0c35247911f890
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-13 02:08:37 +0100

    [feat] import and destructuring props

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,15 +1,19 @@
 import { Link } from "react-router-dom"
 
-const Card = () => {
+const Card = ({ propCard }) => {
+
+    // Destructuring delle props
+    const { id, title, director, genre, release_year, abstract, image } = propCard
+
     return (
 
-        <div class="card">
-            <img src="http://localhost:3000/movies_cover/inception.jpg" class="card-img-top" alt="..." />
-            <div class="card-body">
-                <h5 class="card-title">Card title</h5>
-                <address><i>Lorem ipsum dolor sit amet.</i></address>
-                <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
-                <Link to="movies/1" class="btn btn-primary">Go somewhere</Link>
+        <div className="card">
+            <img src={image} className="card-img-top" alt={title} />
+            <div className="card-body">
+                <h5 className="card-title">{title}</h5>
+                <address><i>Directed by {director}</i></address>
+                <p className="card-text">{abstract}</p>
+                <Link to={`movies/${id}`} className="btn btn-primary">Go somewhere</Link>
             </div>
         </div>
     )
