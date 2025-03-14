be2bfddb28977b31dc24c979840bd82fb4bc3bce
PaoloVisone <polvis.pv.22@gmail.com>
2025-03-14 01:05:50 +0100

    [feat] add logical operetor

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -8,11 +8,14 @@ const Card = ({ propCard }) => {
     return (
 
         <div className="card m-4">
-            <img src={image} className="card-img-top" alt={title} />
+            {/* Se img non esiste */}
+            {image && <img src={image} className="card-img-top" alt={title} />}
             <div className="card-body">
                 <h5 className="card-title">{title}</h5>
-                <address><i>Directed by {director}</i></address>
-                <p className="card-text">{abstract}</p>
+                {/* Se director non esiste */}
+                <address><i>Directed by {director || "Anonymous"}</i></address>
+                {/* Se abstract non esiste */}
+                <p className="card-text">{abstract || "No description"}</p>
                 <Link to={`movies/${id}`} className="btn btn-primary">Go somewhere</Link>
             </div>
         </div>
